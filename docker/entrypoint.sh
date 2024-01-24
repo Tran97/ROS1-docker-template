fd61c0964c77540cc80affceb6ecf5e16b78b6b6
Steven <steven.t@outlook.dk>
2024-01-24 11:26:30 +0100

    cleanup

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-# Basic entrypoint for ROS / Colcon Docker containers
+# Basic entrypoint for ROS / catkin Docker containers
 
 # Source ROS 
 source /opt/ros/${ROS_DISTRO}/setup.bash
 echo "Sourced ROS ${ROS_DISTRO}"
 
 # Source the base workspace, if built
-if [ -f /underlay_ws/install/setup.bash ]
+if [ -f /underlay_ws/devel/setup.bash ]
 then
-  source /underlay_ws/install/setup.bash
+  source /underlay_ws/devel/setup.bash
   echo "Sourced underlay workspace"
 fi
 
 # Source the overlay workspace, if built
-if [ -f /overlay_ws/install/setup.bash ]
+if [ -f /overlay_ws/devel/setup.bash ]
 then
-  source /overlay_ws/install/setup.bash
+  source /overlay_ws/devel/setup.bash
   echo "Sourced overlay workspace"
 fi
 
